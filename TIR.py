f2cd629503855a54ad99c6221c9d83464cb1c9ef
SabigBenmumin <totpophy14755@gmail.com>
2024-02-06 21:48:21 +0700

    update TIR.py

--- TIR.py
+++ TIR.py
@@ -1,12 +1,40 @@
 #TIR mean Thailand Inflation Rate
-
 def getCalender():
-    print("print test")
-    print("")
+    dataC = {
+        0 : {
+            "Calendar" : "2024-01-05",
+            "GMT" : "04:00 AM",
+            "Reference" : "Dec",
+            "Actual" : "-0.83%",
+            "Previous" : "0.44%",
+            "Consensus" : "-0.3%",
+            "TEForecast" : "-0.3%"},
+        1 : {
+            "Calendar" : "2024-02-05",
+            "GMT" : "04:00 AM",
+            "Reference" : "Jan",
+            "Actual" : "-0.83%",
+            "Previous" : "0.44%",
+            "Consensus" : "-0.3%",
+            "TEForecast" : "-0.3%"},
+        2 : {
+            "Calendar" : "2024-03-05",
+            "GMT" : "04:00 AM",
+            "Reference" : "Feb",
+            "Actual" : "-0.83%",
+            "Previous" : "0.44%",
+            "Consensus" : "-0.3%",
+            "TEForecast" : "-0.3%"
+        }
+    }
+    key_list = list(dataC.keys())
+    col_list = list(dataC[0].keys())
+    col_len = [12, 10, 11, 8, 10, 11, 12]
+    line_len = sum(col_len) + 9
+    
 
 def getRelated():
     print("print test")
-    print("")
 
 def getSource():
     Source =  "TREADING ECONOMICS"
@@ -23,10 +51,8 @@ def printChoise():
     for line in getChoises():
         print(line)
     
-
 def main():
     while True:
-        print("-"*50)
         printChoise()
         choise = input("input your choise [1-4]: ")
         while choise not in ["1", "2", "3", "4"]:
@@ -34,6 +60,7 @@ def main():
             print("your input out of choise\nplease input again")
             printChoise()
             choise = input("input your choise [1-4]: ")
+        print("-"*75)
         if choise == "1":
             getCalender()
         elif choise == "2":
@@ -43,6 +70,7 @@ def main():
             print(f"{tpl[0]}: {tpl[1]}")
         else:
             break
+        print("-"*75)
         print("")
 
 main()
